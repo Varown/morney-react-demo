a039783e4e521563d16883c8ab85ff063b1ae707
PengMeng <383867421@qq.com>
2020-05-08 20:30:51 +0800

    解决icon引入时报错的问题 icon完善

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,41 +1,56 @@
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react';
+require('../icons/tag.svg');
+require('../icons/money.svg');
+require('../icons/chart.svg');
 
-const NavWrapper=styled.div`
-box-shadow:0 0 3px rgba(0,0,0,0.25);
-line-height: 24px;
->ul{
-display:flex;
->li{
-width: 33.3333333%;
-text-align: center;
-padding: 16px;
-}
-}
+const NavWrapper = styled.nav`
+  line-height: 24px;
+  box-shadow: 0 0 3px rgba(0,0,0,0.25);
+  > ul {
+    display:flex;
+    > li{
+      width: 33.3333%;
+      text-align:center;
+      display: flex;
+      flex-direction: column;
+      padding: 4px 0;
+      justify-content: center;
+      align-items: center;
+      .icon {
+        width: 24px;      
+        height: 24px;
+      }
+    }
+  }
+`;
 
-`
-
-const Nav=()=>{
-  return(
+const Nav = () => {
+  return (
     <NavWrapper>
       <ul>
         <li>
-          <Link to="/tags">标签</Link>
+          <svg className="icon">
+            <use xlinkHref="#tag" />
+          </svg>
+          <Link to="/tags">标签页</Link>
         </li>
         <li>
-          <Link to="/money">记账</Link>
+          <svg className="icon" >
+            <use xlinkHref="#money" />
+          </svg>
+          <Link to="/money">记账页</Link>
         </li>
         <li>
-          <Link to="/statistics">统计</Link>
+          <svg className="icon" >
+            <use xlinkHref="#chart" />
+          </svg>
+          <Link to="/statistics">统计页</Link>
         </li>
       </ul>
-
     </NavWrapper>
+  );
+};
 
-
-  )
-
-
-}
-export  default Nav;
+export default Nav;
