66451d8d0253e38b760fe7b8030e96533087ac01
PengMeng <383867421@qq.com>
2020-05-09 15:32:52 +0800

    创建/tags/：tag 路由 学会exact  用法

--- src/App.tsx
+++ src/App.tsx
@@ -12,19 +12,23 @@ import Tags from './views/Tags';
 import Money from './views/Money';
 import Statistics from './views/Statistics';
 import NoMatch from './views/NoMatch';
+import {Tag} from './views/Tag';
 
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/tags">
+        <Route path="/tags" exact>
           <Tags/>
         </Route>
-        <Route path="/money">
+        <Route path="/tags/:tag" exact>
+          <Tag/>
+        </Route>
+        <Route path="/money" exact>
           <Money/>
         </Route>
-        <Route path="/statistics">
+        <Route path="/statistics" exact>
           <Statistics/>
         </Route>
         <Redirect exact from="/" to="/money"/>
--- src/components/icon.tsx
+++ src/components/icon.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 require('../icons/tag.svg');
 require('../icons/money.svg');
 require('../icons/chart.svg');
+require('../icons/left.svg');
+require('../icons/right.svg');
 // let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
 // try {importAll(require.context('icons', true, /\.svg$/));} catch (error) {console.log(error);}
 type Props={
