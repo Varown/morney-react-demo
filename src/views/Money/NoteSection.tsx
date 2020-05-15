b2ac6224403095cdea844e8c5ab2e46a9b2dfdfb
PengMeng <383867421@qq.com>
2020-05-16 06:56:29 +0800

    创建Button和Input组件

--- src/views/Money/NoteSection.tsx
+++ src/views/Money/NoteSection.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
-import React, {useRef} from 'react';
+import React, {ChangeEventHandler} from 'react';
+import {Input} from '../../components/Input';
 
 const Wrapper = styled.section`
   background: #f5f5f5;
@@ -23,25 +24,12 @@ type Props = {
   onChange: (value:string) => void
 }
 const NoteSection: React.FC<Props> = (props) => {
-  const node=props.value
-
-  const refInput = useRef<HTMLInputElement>(null);
-  const onBlur = () => {
-    if (refInput.current !== null) {
-      props.onChange(refInput.current.value);
-    }
-  };
+  const note = props.value;
+  const onChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    props.onChange(e.target.value);}
   return (
     <Wrapper>
-      <label>
-        <span>备注</span>
-        <input type="text"
-               placeholder="在这里添加备注"
-               ref={refInput}
-               defaultValue={node}
-               onBlur={onBlur}
-        />
-      </label>
+      <Input label="备注" type="text" value={note} onChange={onChange}/>
     </Wrapper>
   );
 
