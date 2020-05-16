15a836c619847f288d958bc262eaea9d33d01ab6
PengMeng <383867421@qq.com>
2020-05-16 10:16:37 +0800

    实现回退

--- src/components/icon.tsx
+++ src/components/icon.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import cs from 'classnames';
 require('../icons/tag.svg');
 require('../icons/money.svg');
 require('../icons/chart.svg');
@@ -8,12 +9,12 @@ require('../icons/left.svg');
 // try {importAll(require.context('icons', true, /\.svg$/));} catch (error) {console.log(error);}
 type Props={
   name?:string
-}
+} & React.SVGAttributes<SVGElement>
 
 const Icon=(props:Props)=>{
-
+const {name,children,className,...rest}=props
   return(
-    <svg className="icon" >
+    <svg className={cs('icon', className)} {...rest}>
       {props.name &&<use xlinkHref={'#'+props.name} />}
     </svg>
   );
